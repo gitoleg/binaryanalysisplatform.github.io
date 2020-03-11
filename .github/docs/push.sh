a943db562f62f7ba8264e1cc4c49e053bb6ddd39
Oleg <forown@yandex.ru>
2020-03-11 13:21:40 -0400

    test

---
+++ .github/docs/push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+TOKEN=$1
+
+if [ -f bap_commit ]; then
+    repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+    git config --global user.name $GITHUB_ACTOR
+    git config --global user.email "action-noreply@github.com"
+
+    git add bap/api
+    git commit -m "cat bap_commit"
+    git push $remote_repo master
+fi
--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -1,37 +1,26 @@
 #!/usr/bin/env sh
 
-ls -l /drive
-exit 0
+DRIVE=/drive
 
-TOKEN=$1
-io_commit=`git log --pretty=format:"%s" | head -n 1`
+if [ -f $DRIVE/commit ]; then
 
-x=`which docker`
-echo "docker ?$x"
+    io_commit=`cat $DRIVE/commit`
+    bap="bap.upstream"
+    git clone https://github.com/gitoleg/bap --single-branch --branch=new-documentation --depth=1 $bap
+    cd $bap
+    bap_commit=`git rev-parse --short HEAD`
 
-echo "let's look what do we have"
-ls -la
+    if [ "$io_commit" != "bap_commit" ]; then
+        make doc -C $bap
 
-bap="bap.upstream"
-git clone https://github.com/BinaryAnalysisPlatform/bap --single-branch --branch=master --depth=1 $bap
-cd $bap
-bap_commit=`git rev-parse --short HEAD`
+        echo bap_commit > $DRIVE/bap_commit
 
-if [ "$io_commit" != "bap_commit" ]; then
-    make doc -C $bap
+        rsync -av  --delete $bap/doc/man1/ $DRIVE/bap/api/man1/
+        rsync -av  --delete $bap/doc/man3/ $DRIVE/$bap/api/man3/
+        rsync -av  --delete $bap/doc/lisp/ $DRIVE/bap/api/lisp/
+        rsync -avL --delete $bap/doc/odoc/ $DRIVE/bap/api/odoc/
 
-    remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    git config --global user.name $GITHUB_ACTOR
-    git config --global user.email "action-noreply@github.com"
-
-    rsync -av  --delete $bap/doc/man1/ bap/api/man1/
-    rsync -av  --delete $bap/doc/man3/ bap/api/man3/
-    rsync -av  --delete $bap/doc/lisp/ bap/api/lisp/
-    rsync -avL --delete $bap/doc/odoc/ bap/api/odoc/
-
-    git add bap/api
-    git commit -m "$bap_commit"
-    git push $remote_repo master
-else
-    echo "Nothing we need to do, documentation is up-to-date"
+    else
+        echo "Nothing we need to do, documentation is up-to-date"
+    fi
 fi
