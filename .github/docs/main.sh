2012ebb9ad5750fb5f2824d7f8c1e17e2512ae00
Oleg <forown@yandex.ru>
2020-03-12 13:12:19 -0400

    try again

--- .github/docs/main.sh
+++ .github/docs/main.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env sh
 
-x=`cat .github/docs/bap-digest`
-y=`curl -s https://hub.docker.com/v2/repositories/binaryanalysisplatform/bap/tags | jq -r '.results|.[]| select ( .name == "latest") | .images | .[] | .digest'`
+digest=.github/docs/bap-digest
 
-if [ "get$x" != "get$y" ]; then
-    echo y > .github/docs/bap-digest
+old=`cat $digest`
+new=`curl -s https://hub.docker.com/v2/repositories/binaryanalysisplatform/bap/tags | jq -r '.results|.[]| select ( .name == "latest") | .images | .[] | .digest'`
+
+if [ "get$old" != "get$new" ]; then
+    echo $new > $digest
     git log --pretty=format:"%s" | head -n 1 > .github/docs/commit
     docker image build .github/docs -t docs
     mkdir ready
+
+    bap=mycontainer:/home/opam/bap.master
+
     docker run -v `pwd`:/drive --name mycontainer docs
-    docker cp    mycontainer:/home/opam/bap.master/doc/lisp ready/
-    docker cp    mycontainer:/home/opam/bap.master/doc/man1 ready/
-    docker cp    mycontainer:/home/opam/bap.master/doc/man3 ready/
-    docker cp -L mycontainer:/home/opam/bap.master/doc/odoc ready/
-    docker cp    mycontainer:/home/opam/bap.master/bap_commit .
+    docker cp    $bap/doc/lisp ready/
+    docker cp    $bap/doc/man1 ready/
+    docker cp    $bap/doc/man3 ready/
+    docker cp -L $bap/doc/odoc ready/
+    docker cp    $bap/bap_commit .
     docker container stop mycontainer
     docker container rm mycontainer
     docker image rm docs
+else
+    echo "The digest of bap:latest is the same, nothing to do"
 fi
