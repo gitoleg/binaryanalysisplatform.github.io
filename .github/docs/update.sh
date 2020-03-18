52e34d77659edcf9b27b7934c296df89f108d322
oleg <forown@yandex.ru>
2020-03-18 11:07:03 -0400

    try this

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -2,25 +2,43 @@
 
 set -eu
 
-io_commit=/home/opam/commit
+sync () {
+    if [ -d $1 ]; then
+        rsync -a --delete $1 $2
+    fi
+}
+
 
 eval $(opam env)
-ls -l
 
-if [ -f $io_commit ]; then
-    bap="bap.master"
-    git clone https://github.com/BinaryAnalysisPlatform/bap --single-branch --branch=master --depth=1 $bap
-    cd $bap
-    bap_commit=`git rev-parse --short HEAD`
+TOKEN=$1
 
-    if [ "$io_commit" != "$bap_commit" ]; then
-        make doc
-        ls doc
-        echo $bap_commit > bap_commit
-    else
-        echo "Nothing we need to do, documentation is up-to-date"
-    fi
-else
-    echo "Can't find a file with last binaryanalysisplatform.github.io commit"
-    exit 1
-fi
+bap="bap.master"
+
+git clone https://github.com/BinaryAnalysisPlatform/bap --single-branch --branch=master --depth=1 $bap
+cd $bap
+bap_commit=`git rev-parse --short HEAD`
+
+make doc
+ls doc
+
+blog=blog
+
+git clone https://github.com/BinaryAnalysisPlatform/binaryanalysisplatform.github.io --single-branch --branch=master --depth=1 $blog
+
+sync ready/man1/ $blog/bap/api/man1/
+sync ready/man3/ $blog/bap/api/man3/
+sync ready/lisp/ $blog/bap/api/lisp/
+sync ready/odoc/ $blog/bap/api/odoc/
+
+cd $blog
+
+repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config --global user.name $GITHUB_ACTOR
+git config --global user.email "action-noreply@github.com"
+
+git add bap/api
+
+git commit -m $bap_commit
+#  git push $repo master # TODO!
+git push $repo add-actions
